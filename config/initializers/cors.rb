738b1df6fe9d24547d3472b3a55feb3736804ace
maru0pengin <maru0pengin@gmail.com>
2021-08-12 19:08:56 +0900

    fix:corsの設定を修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost\:\d+/, /sabun-kanri-backend.herokuapp.com/
+    origins /localhost\:\d+/, /sabun-kanri-frontend.herokuapp.com/
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
