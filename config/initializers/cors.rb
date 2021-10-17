79d4443cdc6ffd096a0d22dfa29fcc868879a2a7
maru0pengin <maru0pengin@gmail.com>
2021-10-17 18:12:41 +0900

    一旦全てのオリジンを許可

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost\:\d+/, 'https://sabun-kanri-frontend.herokuapp.com/'
+    '*'
+    # origins /localhost\:\d+/, /sabun-kanri-frontend.herokuapp.com/
 
     resource '*',
       headers: :any,
